479de22bc08f38b3dfa3c8fbe6592b3580f93bd9
mirzaakhmedov20 <mirzaakhmedovshukurullo20gmail.com>
2023-12-07 15:24:42 +0500

    JavaScript masalalar

---
+++ index.js
@@ -0,0 +1,57 @@
+//  Masalalar
+
+// 1-masala
+// 1. Foydalanuvchidan 3 ta son kiritishini sorang. Va ushbu sonlarning yigindisini toping
+// let num1 = +prompt("1-sonni kiriting:");
+// let num2 = +prompt("2-sonni kiriting:");
+// let num3 = +prompt("3-sonni kiriting:");
+// let result = num1 + num2 + num3;
+// console.log(alert("Yig'indi:" + result));
+
+// 2-masala
+// 2. Foydalanuvchidan 2 ta son kiritishini sorang. Va ushbu sonlar kvadratlarining yigindisini toping
+// let num1 = +prompt("Birinchi sonni kiriting: ");
+// let num2 = +prompt("Ikkinchi sonni kiriting: ");
+// let result = (num1 + num2) * 2;
+// console.log(alert("Yig'indisi: " + result));
+
+// 3-masala
+// 3. Ixtiyoriy togri tortburchakning perimetrini topuvchi algoritm yozing.
+// let num1 = +prompt("1-sonni kiriting:");
+// let num2 = +prompt("2-sonni kiriting:");
+// let num3 = +prompt("3-sonni kiriting:");
+// let num4 = +prompt("4-sonni kiriting:");
+// let result = num1 + num2 + num3 + num4;
+// console.log(alert("Yig'indi:" + result));
+
+// function calculateRectanglePerimeter(length, width) {
+//   return 2 * (length + width);
+// }
+// let length = parseInt(prompt("Tortburchak uzunligini kiriting: "));
+// let width = parseInt(prompt("Tortburchak kengligini kiriting: "));
+// let perimeter = calculateRectanglePerimeter(length, width);
+// console.log(alert("Tortburchak perimetri: " + perimeter));
+
+// 4-masala
+// Kubning tomoni berilsa uning hajmi hamda to'la sirtini topuvchi algoritm yozing
+// function calculateCubeVolume(side) {
+//   return side ** 3;
+// }
+// function calculateCubeSurfaceArea(side) {
+//   return 6 * (side ** 2);
+// }
+// let sideLength = parseInt(prompt("Kub tomonini kiriting: "));
+// let volume = calculateCubeVolume(sideLength);
+// let surfaceArea = calculateCubeSurfaceArea(sideLength);
+// console.log(alert("Kub hajmi: " + volume));
+// console.log(alert("Kub to'la sirti: " + surfaceArea));
+
+// 5-masala
+// 5. Doiraning diametri berilgan holatda uning yuzini topuvchi algoritm yozing
+// function calculateCircleArea(diameter) {
+//   let radius = diameter / 2;
+//   return Math.PI * (radius ** 2);
+// }
+// let diameter = parseInt(prompt("Doira diametrini kiriting: "));
+// let area = calculateCircleArea(diameter);
+// console.log(alert("Doira yuzi: " + area));
